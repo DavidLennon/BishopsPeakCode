3fa634cdbd5e1745b794f69c7a03bbdeeb5dbf2a
Finlander912 <Finlander912@gmail.com>
2017-01-24 23:29:53 +0000

    Initial upload of testing code

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print "Testing... 1...2...3"
+import serial
+import pynmea2
+
+def test():
+    serialStream = serial.Serial("/dev/ttyUSB1", 9600, timeout=0.5)
+
+    while True:
+        sentance = serialStream.readline()
+        if sentance.find('RMC') > 0:
+            if hasattr(sentance, 'lon'):
+                print sentance
+
+
+#Main function
+if __name__ == "__main__":
+    test()
